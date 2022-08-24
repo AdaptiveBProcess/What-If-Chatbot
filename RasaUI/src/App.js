43025bde9ae5833eff28d5265937005299f78bc9
Daniel Felipe Barón <df.baron10@uniandes.edu.co>
2022-08-24 17:15:11 -0500

    Include user interface files

---
+++ RasaUI/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Basic from "./components/Basic"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WHAT-IF Scenarios Chatbot</h1>
+      <Basic />
+    </div>
+  );
+}
+
+export default App;
